cbe797cf1b81328883e185779bd3b263c5ec468f
Manu Wallner <manu@supermil.ch>
2021-11-22 21:22:48 -0800

    Have the build script adjust the version number

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+set -eux
+set -o pipefail
+
 swift build --configuration release --arch x86_64 --arch arm64
 
 rm -rf plugin
@@ -5,3 +8,14 @@ mkdir -p plugin
 
 rsync -a ./LRPlugin/ plugin/ExportHEIC.lrplugin
 cp ./.build/apple/Products/Release/LRExportHEIC plugin/ExportHEIC.lrplugin/LRExportHEIC
+
+# Update version with latest git tag
+GIT_VERSION=$(git describe --tags)
+MAJOR_VERSION=$(echo $GIT_VERSION | cut -f 1 -d . | tr -d 'v')
+MINOR_VERSION=$(echo $GIT_VERSION | cut -f 2 -d .)
+PATCH_VERSION=$(echo $GIT_VERSION | cut -f 3 -d .)
+BUILD_NUMBER=$(git rev-list --all --count)
+
+VERSION_SPEC="VERSION = { major=${MAJOR_VERSION}, minor=${MINOR_VERSION}, revision=${PATCH_VERSION}, build=${BUILD_NUMBER} },"
+
+sed -I '' "s/VERSION = .*/$VERSION_SPEC/" plugin/ExportHEIC.lrplugin/Info.lua
