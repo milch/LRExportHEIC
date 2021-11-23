eacbc28031bdf393647a116baf4427d689505d2b
Manu Wallner <manu@supermil.ch>
2021-11-22 20:58:22 -0800

    Adjust built plugin folder

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 swift build --configuration release --arch x86_64 --arch arm64
 
-rm -rf ExportHEIC.lrplugin
-rsync -a ./LRPlugin/ ExportHEIC.lrplugin
-cp ./.build/apple/Products/Release/LRExportHEIC ExportHEIC.lrplugin/LRExportHEIC
+rm -rf plugin
+mkdir -p plugin
+
+rsync -a ./LRPlugin/ plugin/ExportHEIC.lrplugin
+cp ./.build/apple/Products/Release/LRExportHEIC plugin/ExportHEIC.lrplugin/LRExportHEIC
